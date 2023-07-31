a4285973423e381d7a98b581976a56f6eb00458e
N4meN1ck <138298706+N4meN1ck@users.noreply.github.com>
2023-07-31 14:33:08 +0100

    Generate data URL from an html code. Remove extra special characters to reduce the size of the data URL

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
+import base64
+import re
 from flask import Flask
 from flask import render_template
 from flask import request
 app = Flask(__name__)
+
+
+def remove_special_characters(input_string):
+    input_string = re.sub(' +', ' ', input_string)
+    special_characters = ["\r", "\n", "\t"]
+    for char in special_characters:
+        input_string = input_string.replace(char, "")
+    while '> <' in input_string:
+        input_string = input_string.replace('> <', '><')
+    return input_string
 
 
 @app.route("/")
@@ -12,6 +24,11 @@ def hello_world():
 @app.route("/qrcode",methods=["GET", "POST"])
 def qr_code():
     if request.method == "POST":
-        return request.form.get("file", "_")
+        data = request.form.get("file", "_")
+        data = remove_special_characters(data)
+        print(repr(data))
+        data_bytes = data.encode('ascii')
+        base64_bytes = base64.b64encode(data_bytes)
+        return "data:text/html;base64,"+str(base64_bytes)[2:-1]
     if request.method == "GET":
         return ":("
