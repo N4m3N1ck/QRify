5bed3e2b34004eb4f3e488b4bbf8cf96f4ffae40
N4meN1ck <138298706+N4meN1ck@users.noreply.github.com>
2023-07-31 13:09:11 +0100

    Basic website functions

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
+
+@app.route("/qrcode",methods=["GET", "POST"])
+def qr_code():
+    if request.method == "POST":
+        return request.form.get("file", "_")
+    if request.method == "GET":
+        return ":("
