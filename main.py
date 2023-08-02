d08b5db9e2c2ab458f4e5e24044bc8563e987db6
N4meN1ck <138298706+N4meN1ck@users.noreply.github.com>
2023-08-02 10:54:17 +0100

    Add error message when QR code too large. Change the structure of the website

--- main.py
+++ main.py
@@ -22,12 +22,7 @@ def remove_special_characters(input_string):
     return input_string
 
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html")
-
-
-@app.route("/qrcode", methods=["GET", "POST"])
+@app.route("/create/html", methods=["GET", "POST"])
 def qr_code():
     if request.method == "POST":
         data = request.form.get("file", "<h1 style='color:red'>Hello World!</h1>")
@@ -47,11 +42,16 @@ def qr_code():
             data_url = "data:text/html;base64," + str(base64_bytes)[2:-1]
             if len("data:text/html," + url_encode_string(data)) < len(data_url):
                 data_url = "data:text/html," + url_encode_string(data)
-        qr_code_img = qrcode.make(data_url)
-        qr_code_img.save('QRcode.png')
-        with open("QRcode.png", "rb") as image_file:
-            encoded_string = base64.b64encode(image_file.read())
-        return render_template("QRcode.html", url=data_url, img="data:image/png;base64," + str(encoded_string)[2:-1],
-                               code=code)
+        img_data = ""
+        try:
+            qr_code_img = qrcode.make(data_url)
+            qr_code_img.save('QRcode.png')
+            with open("QRcode.png", "rb") as image_file:
+                encoded_string = base64.b64encode(image_file.read())
+            img_data = "data:image/png;base64," + str(encoded_string)[2:-1]
+        except ValueError:
+            img_data = "error"
+        return render_template("htmlqrready.html", url=data_url, img=img_data,
+                               code=code,req="POST")
     if request.method == "GET":
-        return ":("
+        return render_template("htmlqrready.html",req="GET")
