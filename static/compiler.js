f15ff6194c003fda29846f094c35f575c570f2c4
N4meN1ck <138298706+N4meN1ck@users.noreply.github.com>
2023-08-08 22:15:14 +0100

    Improve CSS

--- static/compiler.js
+++ static/compiler.js
@@ -11,3 +11,18 @@ function showObject(id,display){
 function hideObject(id){
     document.getElementById(id).style.display="none";
 }
+document.getElementById('file').addEventListener('keydown', function(e) {
+  if (e.key == 'Tab') {
+    e.preventDefault();
+    var start = this.selectionStart;
+    var end = this.selectionEnd;
+
+    // set textarea value to: text before caret + tab + text after caret
+    this.value = this.value.substring(0, start) +
+      "\t" + this.value.substring(end);
+
+    // put caret at right position again
+    this.selectionStart =
+      this.selectionEnd = start + 1;
+  }
+});
