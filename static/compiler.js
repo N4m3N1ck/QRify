a450dc874b904740e6f5a6ac2a8e9a6836d90f24
N4meN1ck <138298706+N4meN1ck@users.noreply.github.com>
2023-08-08 16:23:42 +0100

    Reorginized the we layout

--- static/compiler.js
+++ static/compiler.js
@@ -1,20 +1,13 @@
-function compile(id) {
+function compile(id,outputId) {
     const code = document.getElementById(id).value;
-    var e = document.getElementById("output");
+    var e = document.getElementById(outputId);
     e.contentDocument.open();
     e.contentDocument.write(code);
     e.contentDocument.close();
 }
-function showObject(id){
-    document.getElementById(id).style.display="inline-block";
+function showObject(id,display){
+    document.getElementById(id).style.display=display;
 }
 function hideObject(id){
     document.getElementById(id).style.display="none";
-}
-function hideIfEmpty(id,idToHide){
-    if(document.getElementById(id).value === ""){
-        hideObject(idToHide);
-    }else{
-        showObject(idToHide);
-    }
 }
